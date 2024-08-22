b33d564f034570b1e74f0b453819b34fcce8f8b7
Wandile Kubheka <retroff01@gmail.com>
2024-08-22 21:39:11 +0200

    basic UI

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,58 @@
-import { Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { StatusBar } from 'expo-status-bar'
+import Button from '@/components/Button'
 
-export default function Index() {
+type Props = {}
+
+const Index = (props: Props) => {
+  const [sum,setSum] = useState("hey")
+  const buttons = ['ac','%','d','/','7','8','9','*','4','5','6','-','1','2','3','+','0','.','l','=']
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="light"/>
+      <View style={styles.displayContainer}>
+      <Text style={styles.displayText}>{sum}</Text>
+      </View>
+      <View style={{flex:2}}>
+
+      <FlatList 
+      contentContainerStyle={styles.ButtonsContainer}
+        scrollEnabled={false}
+        numColumns={4}
+        data={buttons}
+        keyExtractor={item => item}
+        showsVerticalScrollIndicator={false}
+        renderItem={({item}) => <Button callback={setSum} text={item} />}
+      />
+      </View>
+
+    </SafeAreaView>
+  )
 }
+
+export default Index
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:'#0D0D0D'
+  },displayText:{
+    color:'white',
+    fontSize:50,
+    fontWeight:'bold',
+    textAlign:'right',
+    alignItems:'center'
+  },displayContainer:{
+    justifyContent:'center',
+    paddingRight:40,
+    flex:1
+
+  },ButtonsContainer:{
+    justifyContent:'space-between',
+    flex:1,
+  }
+
+})
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,14 +1,35 @@
-import { View, Text } from 'react-native'
+import { View, Text,TouchableOpacity, StyleSheet, useWindowDimensions } from 'react-native'
 import React from 'react'
 
-type Props = {}
+type Props = {
+    text: any,
+    callback: any
+}
 
-const Button = (props: Props) => {
+const Button = ({text,callback}: Props) => {
+    const {width,height} = useWindowDimensions()
+    const buttonSize = width/5
+    const padding = buttonSize /5 
   return (
-    <View>
-      <Text>Button</Text>
-    </View>
+    <TouchableOpacity style={[styles.container,{marginLeft:padding,width:buttonSize}]}>
+      <Text style={styles.text}>{text}</Text>
+    </TouchableOpacity>
   )
 }
 
 export default Button
+
+
+const styles = StyleSheet.create({
+    container:{
+        aspectRatio:1,
+        backgroundColor:'green',
+        alignItems:'center',
+        justifyContent:'center',
+        borderRadius:999
+
+    },text:{
+        fontWeight:"condensedBold",
+        fontSize:25,
+    }
+})
